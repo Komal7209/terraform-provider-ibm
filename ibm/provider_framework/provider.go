5b0d79e49db5b629e8010278b80798b436d2f0bd
komal7209 <komal.pal_7@yahoo.com>
2026-04-14 11:16:01 +0530

    chore: Updated provider for byoc_bearer_token
    Signed-off-by: komal7209 <komal.pal_7@yahoo.com>

--- ibm/provider_framework/provider.go
+++ ibm/provider_framework/provider.go
@@ -57,6 +57,7 @@ type frameworkProviderModel struct {
 	IAMProfileName         types.String `tfsdk:"iam_profile_name"`
 	IAMToken               types.String `tfsdk:"iam_token"`
 	IAMRefreshToken        types.String `tfsdk:"iam_refresh_token"`
+	BYOCBearerToken        types.String `tfsdk:"byoc_bearer_token"`
 	Visibility             types.String `tfsdk:"visibility"`
 	PrivateEndpointType    types.String `tfsdk:"private_endpoint_type"`
 	EndpointsFilePath      types.String `tfsdk:"endpoints_file_path"`
@@ -184,6 +185,11 @@ func (p *frameworkProvider) Schema(ctx context.Context, req provider.SchemaReque
 			"iam_refresh_token": schema.StringAttribute{
 				Optional:    true,
 				Description: "IAM Authentication refresh token",
+			},
+			"byoc_bearer_token": schema.StringAttribute{
+				Optional:    true,
+				Sensitive:   true,
+				Description: "BYOC API Bearer token (separate from IAM token)",
 			},
 			"visibility": schema.StringAttribute{
 				Optional:    true,
